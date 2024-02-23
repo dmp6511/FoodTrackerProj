0bf3c45713a593b65aa93e494d7c2baa5a9acf32
De'Andre Perry <dmp6511@rit.edu>
2024-02-23 14:54:11 -0500

    package json and eslint

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
 // Server JS
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
 
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+    console.log(request);
+}
+
+
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127..0.01:${port}`);
+})
